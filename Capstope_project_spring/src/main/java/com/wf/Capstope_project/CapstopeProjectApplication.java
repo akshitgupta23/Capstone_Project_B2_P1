a7315d419ed5e0c3b794c81f6cd9d89008b0d818
akshitgupta23 <46861280+akshitgupta23@users.noreply.github.com>
2022-11-03 23:31:08 +0530

    frontend and backend setup

---
+++ Capstope_project_spring/src/main/java/com/wf/Capstope_project/CapstopeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wf.Capstope_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstopeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstopeProjectApplication.class, args);
+	}
+
+}
---
+++ Capstope_project_spring/src/test/java/com/wf/Capstope_project/CapstopeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.Capstope_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstopeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
