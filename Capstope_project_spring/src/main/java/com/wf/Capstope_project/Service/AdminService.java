f453676da3cb5724b4572ac8eb00e7514ab99c46
Akshayarathna <64595814+Akshayarathna@users.noreply.github.com>
2022-11-04 17:22:01 +0530

    Update AdminService.java

--- Capstope_project_spring/src/main/java/com/wf/Capstope_project/Service/AdminService.java
+++ Capstope_project_spring/src/main/java/com/wf/Capstope_project/Service/AdminService.java
@@ -1,12 +1,24 @@
 package com.wf.Capstope_project.Service;
 
 import com.wf.Capstope_project.Dao.AdminDao;
+import com.wf.Capstope_project.Entity.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AdminService {
 
     @Autowired
     AdminDao adminDao;
+
+    public boolean login(Admin UIUser){
+        Optional<Admin> user = adminDao.findById(UIUser.getUser_id());
+        if(user.isPresent() && user.get().getPassword().equals(UIUser.getPassword()))
+        {
+            return true;
+        }
+        return false;
+    }
 }
