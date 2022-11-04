045e0dd648ff1d3abcb7e563bb3ff5e37006671f
aks05 <aksingh2001.p@gmail.com>
2022-11-04 16:42:59 +0530

    Dao classes completed

--- Capstope_project_spring/src/main/java/com/wf/Capstope_project/Dao/BranchDao.java
+++ Capstope_project_spring/src/main/java/com/wf/Capstope_project/Dao/BranchDao.java
@@ -1,7 +1,22 @@
 package com.wf.Capstope_project.Dao;
 
+import com.wf.Capstope_project.Entity.Admin;
+import com.wf.Capstope_project.Entity.Branch;
+import com.wf.Capstope_project.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface BranchDao{
+public interface BranchDao extends JpaRepository<Branch, String> {
+
+    public List<Branch> findAll();
+
+    void delete(Branch branch);
+
+    Branch save(Branch branch);
+
+    Optional<Branch> findById(String branch_id);
 }
--- Capstope_project_spring/src/main/java/com/wf/Capstope_project/Dao/CustomerDao.java
+++ Capstope_project_spring/src/main/java/com/wf/Capstope_project/Dao/CustomerDao.java
@@ -1,7 +1,23 @@
 package com.wf.Capstope_project.Dao;
 
+import com.wf.Capstope_project.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CustomerDao {
+public interface CustomerDao extends JpaRepository<Customer, String> {
+
+    List<Customer> findAll();
+
+    void delete(Customer customer);
+
+    Customer save(Customer customer);
+
+    void deleteCustomersByBranch_Branch_id(String branch_id);
+
+
+
+
 }
